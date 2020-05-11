31cf10897318dd9e81277d0df5c044c927196954
Apinan Woratrakun <apinan@MacBook-Pro-2018.local>
2020-05-12 01:59:27 +0700

    add cookie function & consent template

--- pdpa-consent.php
+++ pdpa-consent.php
@@ -21,16 +21,75 @@ if ( !function_exists( 'add_action' ) ) {
 	exit;
 }
 
+define( '_PATH', plugin_dir_path( __FILE__ ) );
 
-class PDPA_Consent {
-    private $ver;
-    private $show_popup = true;
-    private $consent_popup_title = '';
-    private $consent_popup_button_text = '';
+// Check get_plugin_data exists.
+if( !function_exists('get_plugin_data') ){
+    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+}
 
-    public function __construct () {
+if(!class_exists('PDPA_Consent')){
+    class PDPA_Consent {
+        private $ver;
+        private $show_popup = true;
+        private $consent_popup_title = '';
+        private $consent_popup_button_text = '';
+        private $plugin_info;
+
+        public function __construct () {
+            $this->plugin_info = get_plugin_data( _PATH . 'pdpa-consent.php' );
+            $this->initial();
+        }
+
+        public function initial() {
+            add_filter( 'body_class', array( $this, 'change_body_class' ) );
+
+            add_action( 'wp_enqueue_scripts', array( $this, 'pdpa_enqueue_scripts' ) );
+        }
+
+        public function pdpa_enqueue_scripts() {
+            wp_enqueue_script( 'pdpa-consent', plugins_url( 'assets/pdpa-consent.js', __FILE__ ), array(), $this->plugin_info['Version'] );
+            wp_enqueue_style( 'pdpa-consent', plugins_url( 'assets/pdpa-consent.css', __FILE__ ), array(), $this->plugin_info['Version'] );
+        }
+
+        public function pdpa_cookies_set() {
+            return apply_filters( 'cn_is_cookie_set', isset( $_COOKIE['pdpa_accepted'] ) );
+        }
+
+        public static function pdpa_cookies_accepted() {
+            return apply_filters( 'cn_is_cookie_accepted', isset( $_COOKIE['pdpa_accepted'] ) && $_COOKIE['pdpa_accepted'] === 'true' );
+        }
+
+        public function change_body_class( $classes ) {
+            if ( is_admin() )
+                return $classes;
+
+            if ( $this->pdpa_cookies_set() ) {
+                $classes[] = 'pdpa-set';
+
+                if ( $this->pdpa_cookies_accepted() )
+                    $classes[] = 'pdpa-accepted';
+                else
+                    $classes[] = 'pdpa-refused';
+            } else
+                $classes[] = 'pdpa-not-set';
+
+            return $classes;
+        }
+
+        public function create_notice() {
+
+        }
+
+        private function pdpa_admin_menu() {
+
+        }
+
+        private function pdpa_admin_option() {
+
+        }
+
 
     }
-
-
+    new PDPA_Consent;
 }
